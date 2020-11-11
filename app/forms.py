4bce034e0ca75d46d86a931bb6b9542ed40756b0
Ahmed Yasser Asaker <Ahmed.Yasser@GACEGY.LOCAL>
2020-11-11 13:17:02 +0200

    Adding some Features

--- app/forms.py
+++ app/forms.py
@@ -8,19 +8,22 @@ class StockCreateForm(forms.ModelForm):
         model = Stock
         fields = ['category', 'item_name', 'quantity']
         
-       def clean_category(self):
-              category = self.cleaned_data.get('category')
-              if not category:
-                     raise forms.ValidationError('This field is required')
-              for instance in Stock.objects.all():
-                     if instance.category == category:
-                            raise forms.ValidationError(str(category) + ' is already created')
-              return category
+       # def clean_category(self):
+       #        category = self.cleaned_data.get('category')
+       #        if not category:
+       #               raise forms.ValidationError('This field is required')
+       #        for instance in Stock.objects.all():
+       #               if instance.category == category:
+       #                      raise forms.ValidationError(str(category) + ' is already created')
+       #        return category
 
        def clean_item_name(self):
               item_name = self.cleaned_data.get('item_name')
               if not item_name:
                      raise forms.ValidationError('This field is required')
+              for instance in Stock.objects.all():
+                     if instance.item_name == item_name:
+                            raise forms.ValidationError(str(item_name) + ' is already created')
               return item_name
 
 class StockHistorySearchForm(forms.ModelForm):
